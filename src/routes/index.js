a56a217c71c6a2aabd6e5fef7ed0d8fad58f4331
Rakesh Ravuri <rravuri@gmail.com>
2020-10-27 12:20:25 +0530

    react app initial commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+
+var indexhtml;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  if (!indexhtml) {
+    indexhtml = fs.readFileSync('uiapp/build/index.html');
+  }
+
+  res.send(indexhtml.toString());
 });
 
 module.exports = router;
